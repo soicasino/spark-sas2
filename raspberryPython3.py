dc1579e372ae7aefbe360c971e5997efbfb0ae1f
Mehmet BAĞCI <mehmet.bagci@gmail.com>
2025-06-03 11:18:49 +0300

    update raspberryPython3.py to correct variable name from 'files' to 'dirs' in find function; improve clarity of directory traversal logic

--- raspberryPython3.py
+++ raspberryPython3.py
@@ -4333,7 +4333,7 @@ def ReadAssetToInt(d):
 import os, fnmatch
 def find(pattern, path):
     result = []
-    for root,  files in os.walk(path):
+    for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
